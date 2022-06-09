08da72e7358ab43921f15498952b0a167c603f34
GGB <1223416496@qq.com>
2022-06-10 01:32:14 +0800

    Init student

--- Student/src/main/java/com/cfive/classroom/student/ChangePassword.java
+++ Student/src/main/java/com/cfive/classroom/student/ChangePassword.java
@@ -1,58 +1,96 @@
 package com.cfive.classroom.student;
 
+import com.cfive.classroom.library.database.DatabaseHelper;
+import com.cfive.classroom.library.database.util.DependenciesNotFoundException;
+import com.cfive.classroom.library.database.util.NoConfigException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.SQLException;
 
 public class ChangePassword {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final ChangePassword changePassword = new ChangePassword();
+
     private JPanel rootPanel;
-    private JTextField textField1;
+    private JTextField textNum;
     private JButton cancel;
     private JButton confirm;
     private JPasswordField passwordField1;
     private JPasswordField passwordField2;
+    private static JFrame frame = new JFrame("修改密码");
 
-    static JFrame frame = new JFrame("ChangePassword");
+    private String stuNo;
 
     public ChangePassword() {
         confirm.addActionListener(e -> {
             if(check()){
-                Center.frame.setVisible(true);
-                frame.setVisible(false);
+                try {
+                    DatabaseHelper.changePasswdInStudent(Long.parseLong(stuNo), passwordField1.getPassword().toString());
+                } catch (NoConfigException ex) {
+                    JOptionPane.showMessageDialog(null,"没有数据库配置文件","警告",JOptionPane.ERROR_MESSAGE);
+                    LOGGER.error("No configuration", e);
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null,"数据库出错","警告",JOptionPane.ERROR_MESSAGE);
+                    LOGGER.error("SQLException",e);
+                } catch (DependenciesNotFoundException ex) {
+                    LOGGER.error("DependenciesNotFoundException",e);
+                } catch (NoSuchAlgorithmException ex) {
+                    LOGGER.error("NoSuchAlgorithmException",e);
+                } catch (InvalidKeySpecException ex) {
+                    LOGGER.error("InvalidKeySpecException",e);
+                }
+                frame.dispose();
             }
         });
         cancel.addActionListener(e -> {
-            Center.frame.setVisible(true);
-            frame.setVisible(false);
+            frame.dispose();
         });
     }
 
-    public static void main(String[] args) {
 
-        frame.setContentPane(new ChangePassword().rootPanel);
+
+    private boolean check() {
+        String password1,password2,num;
+        num = String.valueOf(textNum.getText());
+
+        LOGGER.info("传入学号"+stuNo);
+        LOGGER.info(num);
+
+        password1 = String.valueOf(passwordField1.getPassword());
+        password2 = String.valueOf(passwordField2.getPassword());
+        if(num.equals(stuNo)){
+            if (password1.length()==0 || password2.length()==0) {
+                JOptionPane.showMessageDialog(null,"输入的密码为空");
+                return false;
+            } else if (!password1.equals(password2)) {
+                JOptionPane.showMessageDialog(null, "两次输入密码不同");
+                return false;
+            } else return true;
+        }
+        else {
+            JOptionPane.showMessageDialog(null,"学号非本人学号");
+            return false;
+        }
+    }
+
+
+    public void start(String stuNo) {
+        frame.setContentPane(changePassword.rootPanel);
+        frame.setSize(600,400);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        changePassword.stuNo = stuNo;
+        LOGGER.info(changePassword.stuNo);
+    }
+    public static void main(String[] args) {
+        frame.setContentPane(changePassword.rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600,400);
         frame.setVisible(false);
-    }
-
-    public void start() {
-        frame.setContentPane(new ChangePassword().rootPanel);
-        frame.setBounds(650,300,600,400);
-        frame.setResizable(false);
-//        frame.pack();
-        frame.setVisible(true);
-    }
-
-    private boolean check() {
-        String password1,password2;
-        password1 = String.valueOf(passwordField1.getPassword());
-        password2 = String.valueOf(passwordField2.getPassword());
-        if (password1.length()==0 || password2.length()==0) {
-            JOptionPane.showMessageDialog(null,"输入的密码为空");
-            return false;
-        } else if (password1.equals(password2) == false) {
-            JOptionPane.showMessageDialog(null, "两次输入密码不同");
-            return false;
-        } else return true;
     }
 }
--- Student/src/main/java/com/cfive/classroom/student/MainWindow.java
+++ Student/src/main/java/com/cfive/classroom/student/MainWindow.java
@@ -1,47 +1,78 @@
 package com.cfive.classroom.student;
 
+import com.cfive.classroom.library.database.DatabaseHelper;
+import com.cfive.classroom.library.database.bean.Student;
+import com.cfive.classroom.library.database.util.DependenciesNotFoundException;
+import com.cfive.classroom.library.database.util.NoConfigException;
+import com.formdev.flatlaf.FlatLightLaf;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.SQLException;
 
 public class MainWindow{
-
+    private static final MainWindow mainWindow = new MainWindow();
     private JPanel rootPanel;
     private JLabel title;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
+    private JTextField stuNoText;
+    private JPasswordField passwordText;
     private JButton login;
-    static JFrame frame = new JFrame("学生登录界面");
+    private JLabel password;
+    private static JFrame frame = new JFrame("学生登录界面");
+    private static final Logger LOGGER = LogManager.getLogger();
     public MainWindow() {
+        //登录按钮
         login.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(check()){
                     Center center = new Center();
-                    center.start();
-                    frame.setVisible(false);
+                    center.start(String.valueOf(stuNoText.getText()));
+                    frame.dispose();
                 }
             }
         });
-
     }
-
     public boolean check(){
-        String password = new String(passwordField1.getPassword());
-        if (textField1.getText() == null || password.length() == 0) {
+        String stuPassword = new String(passwordText.getPassword());
+        String stuNo = new String(stuNoText.getText());
+        LOGGER.info(Long.valueOf(stuNo));
+        //判断密码
+        if (stuNo.length() == 0 || stuPassword.length() == 0) {
             JOptionPane.showMessageDialog(null, "账号密码不能为空");
             return false;
         } else {
-            return true;
+            boolean checkPassword=false;
+            try {
+                checkPassword=DatabaseHelper.checkPasswdInStudent(Long.parseLong(stuNo),stuPassword);
+            } catch (NoConfigException e) {
+                JOptionPane.showMessageDialog(null,"没有数据库配置文件","警告",JOptionPane.ERROR_MESSAGE);
+                LOGGER.error("No configuration", e);
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null,"数据库出错","警告",JOptionPane.ERROR_MESSAGE);
+                LOGGER.error("SQLException",e);
+            } catch (DependenciesNotFoundException e) {
+                LOGGER.error("DependenciesNotFoundException",e);
+            } catch (NoSuchAlgorithmException e) {
+                LOGGER.error("NoSuchAlgorithmException",e);
+            } catch (InvalidKeySpecException e) {
+                LOGGER.error("InvalidKeySpecException",e);
+            }
+            return checkPassword;
         }
     }
-
     public static void main(String[] args) {
-
-        frame.setContentPane(new MainWindow().rootPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.pack();
-        frame.setBounds(650,300,600,400);
+        FlatLightLaf.setup();
+        frame.setContentPane(mainWindow.rootPanel);
+        frame.setSize(600,400);
+        frame.setTitle("登录");
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setResizable(false);
     }
