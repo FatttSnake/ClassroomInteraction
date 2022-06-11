7cacbee65c83303b94ebd7063c7f2babf4e19b24
FatttSnake <fatttsnake@gmail.com>
2022-06-12 02:24:41 +0800

    Fixed socket bug

--- Library/src/main/java/com/cfive/classroom/library/net/StudentNet.java
+++ Library/src/main/java/com/cfive/classroom/library/net/StudentNet.java
@@ -2,6 +2,7 @@ package com.cfive.classroom.library.net;
 
 import com.cfive.classroom.library.net.util.MessageObject;
 import com.cfive.classroom.library.net.util.ReceiveListener;
+import com.cfive.classroom.library.net.util.SocketHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,25 +11,27 @@ import java.net.Socket;
 
 public class StudentNet {
     private static final Logger LOGGER = LogManager.getLogger();
-    private final Socket socket;
+    private final SocketHandler socketHandler;
 
     public StudentNet(String host, int port) throws IOException {
-        socket = new Socket(host, port);
+        Socket socket = new Socket(host, port);
+        socketHandler = new SocketHandler(socket, null);
+        socketHandler.start();
+    }
+
+    public StudentNet(String host, int port, ReceiveListener receiveListener) throws IOException {
+        Socket socket = new Socket(host, port);
+        socketHandler = new SocketHandler(socket, receiveListener);
+        socketHandler.start();
+    }
+
+    public void setOnReceiveListener(ReceiveListener receiveListener) {
+        socketHandler.setOnReceiveListener(receiveListener);
     }
 
     //发送
-    public void sendMessageThread(MessageObject messageObject) {
-        SendThread sendThread = new SendThread(socket, messageObject);
-        sendThread.start();
+    public void sendMessage(MessageObject messageObject) {
+        socketHandler.sendMessage(messageObject);
     }
-
-    //接受信息
-    public void setOnReceiveListener(ReceiveListener receiveListener)
-    {
-        ReceiveThread receiveThread = new ReceiveThread(socket);
-        receiveThread.setOnReceiveListener(receiveListener);
-        receiveThread.start();
-    }
-
 
 }
--- Library/src/test/java/com/cfive/classroom/library/net/StudentTest.java
+++ Library/src/test/java/com/cfive/classroom/library/net/StudentTest.java
@@ -1,47 +1,52 @@
 package com.cfive.classroom.library.net;
 
+import com.cfive.classroom.library.database.bean.AttStatus;
 import com.cfive.classroom.library.net.util.MessageObject;
+import com.cfive.classroom.library.net.util.MessageType;
 import com.cfive.classroom.library.net.util.ReceiveListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class StudentTest {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    /*@Test
-    void studentListenterTest()  {
-        StudentNet studentNet = new StudentNet();
-        try {
-            studentNet.socketConnect("localhost",8888);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        studentNet.receiveMessageThread(new ReceiveListener() {
-            @Override
-            public void onReceive(MessageObject messageObject) {
-                LOGGER.info(messageObject.getCode());
-            }
+    @Test
+    void teacherTest() throws IOException {
+        TeacherNet teacherNet = new TeacherNet(8888);
+        teacherNet.setOnReceiveListener(messageObject -> {
+            LOGGER.debug(messageObject);
+            teacherNet.sendAllMessage(messageObject);
         });
-        while(true);
+
+        while (true);
     }
 
+    @Test
+    void studentTest() throws IOException {
+        StudentNet studentNet = new StudentNet("localhost", 8888);
+        studentNet.setOnReceiveListener(LOGGER::debug);
+        studentNet.sendMessage(new MessageObject("stuNO", "stuName", "code", "message", "count", AttStatus.not_signed, LocalDateTime.now(), MessageType.Chat));
+        while (true);
+    }
 
     @Test
-    void teacherListenterTes() {
-        TeacherNet teacherNet = new TeacherNet();
-        try {
-            teacherNet.socketConnect(8888);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    void studentTest1() throws IOException {
+        StudentNet studentNet = new StudentNet("localhost", 8888);
+        studentNet.setOnReceiveListener(LOGGER::debug);
+        studentNet.sendMessage(new MessageObject("stuNO", "stuName", "code", "message", "count", AttStatus.not_signed, LocalDateTime.now(), MessageType.Chat));
+        while (true);
+    }
 
-        MessageObject messageObject = new MessageObject();
-        messageObject.setCode("123");
-        teacherNet.sendMessageThread(messageObject);
-        while(true);
-    }*/
+    @Test
+    void studentTest2() throws IOException {
+        StudentNet studentNet = new StudentNet("localhost", 8888);
+        studentNet.setOnReceiveListener(LOGGER::debug);
+        studentNet.sendMessage(new MessageObject("stuNO", "stuName", "code", "message", "count", AttStatus.not_signed, LocalDateTime.now(), MessageType.Chat));
+        studentNet.sendMessage(new MessageObject("stuNO", "stuName", "code", "message", "count", AttStatus.not_signed, LocalDateTime.now(), MessageType.Chat));
+        while (true);
+    }
 }
